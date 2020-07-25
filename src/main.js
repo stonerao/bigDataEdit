d0bb250c7736a89147e73c943215b5f2d85b8029
stonerao <stoneraoy@gmail.com>
2020-07-25 12:38:19 +0800

    介绍

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import http from './utils/axios'
 
-Vue.config.productionTip = false
+import './style/reset.less';
+
+Vue.config.productionTip = false;
+Vue.prototype.$http = http;
+
+router.beforeEach((to, from, next) => {
+  next();
+})
+// 路由结束之后执行
+router.afterEach(() => { });
+
 
 new Vue({
   router,
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,25 @@
 /**
- * 2020-7-25
- * 饶岩
- * 用于处理ajax => axios 的封装
- */
+* raoyan
+* 2020-7-25
+* 用于异步ajax
+*/
 
-import axios from 'axios';
+import axios from 'axios'
+axios.interceptors.response.use(res => {
+    const result = res || {}
+    if (result.status === 200) {
+        return result.data
+    }
+});
+
+axios.defaults.withCredentials = true;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.transformRequest = [function (data) {
+    let ret = '';
+    for (let it in data) {
+        ret += `${encodeURIComponent(it)}=${encodeURIComponent(data[it])}&`
+    }
+    return ret;
+}];
+
+export default axios;
